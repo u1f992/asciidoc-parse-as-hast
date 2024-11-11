cd15f18876af229ac9474e39be0d9508245d921f
Koutaro Mukai <mukai.k1011k@outlook.jp>
2024-11-11 12:40:35 +0900

    Update example

--- example/vivliostyle.config.js
+++ example/vivliostyle.config.js
@@ -5,18 +5,26 @@ import rehypeStringify from "rehype-stringify";
 // @ts-check
 /** @type {import('@vivliostyle/cli').VivliostyleConfigSchema} */
 const vivliostyleConfig = {
-    title: "Principia",
-    author: "Isaac Newton",
+    title: "AsciiDoc入門",
+    author: "ChatGPT",
     theme: "./css",
     image: "ghcr.io/vivliostyle/cli:8.16.1",
     entry: ["manuscript.adoc.md"],
     workspaceDir: ".vivliostyle",
-    documentProcessor: (config, metadata) =>
+    documentProcessor: (options, metadata) =>
         unified()
             .use(asciidoctorParseAsHast, {
                 standalone: true,
-                attributes: { lang: "ja", linkcss: false, "stylesheet!": null },
+                attributes: {
+                    lang: "ja",
+                    stylesheet: options.style.join(","),
+                    nofooter: true,
+                },
             })
+            // .use((tree) => {
+            //     console.log(options, metadata);
+            //     return tree;
+            // })
             .use(rehypeStringify),
 };
 
